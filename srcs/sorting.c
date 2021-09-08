709cf788abcb2fa772828bda75f4ad36960f138f
mbllt <mballet@student.42lyon.fr>
2021-09-08 13:41:34 +0200

    norm okok

--- srcs/sorting.c
+++ srcs/sorting.c
@@ -6,7 +6,7 @@
 /*   By: mballet <mballet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 17:40:46 by mballet           #+#    #+#             */
-/*   Updated: 2021/09/08 12:53:09 by mballet          ###   ########.fr       */
+/*   Updated: 2021/09/08 13:41:09 by mballet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,30 @@ static int	sort_three(t_list_int **actual, t_list_int **other,
 	return (1);
 }
 
+static int	send_stack_even(t_list_int **actual, t_list_int **other,
+	t_global *global, int nbr_sort_tmp)
+{
+	global->nbr_sort = nbr_sort_tmp / 2 + nbr_sort_tmp % 2;
+	if (!sorting(actual, other, global, 0))
+		return (0);
+	global->nbr_sort = nbr_sort_tmp / 2;
+	if (!sorting(other, actual, global, 3))
+		return (0);
+	return (1);
+}
+
+static int	send_stack_odd(t_list_int **actual, t_list_int **other,
+	t_global *global, int nbr_sort_tmp)
+{
+	global->nbr_sort = nbr_sort_tmp / 2 + nbr_sort_tmp % 2;
+	if (!sorting(actual, other, global, 1))
+		return (0);
+	global->nbr_sort = nbr_sort_tmp / 2;
+	if (!sorting(other, actual, global, 2))
+		return (0);
+	return (1);
+}
+
 int	sorting(t_list_int **actual, t_list_int **other,
 	t_global *global, int stack)
 {
@@ -56,35 +80,23 @@ int	sorting(t_list_int **actual, t_list_int **other,
 
 	nbr_sort_tmp = global->nbr_sort;
 	if (nbr_sort_tmp <= 3)
-	{
 		return (sort_three(actual, other, global, stack));
-	}
 	if (!(find_median(*actual, global, stack)))
 		return (0);
 	if (!seperate(actual, other, global, stack))
 		return (0);
 	if (!(stack % 2))
 	{
-		global->nbr_sort = nbr_sort_tmp / 2 + nbr_sort_tmp % 2;
-		if (!sorting(actual, other, global, 0))
-			return (0);
-		global->nbr_sort = nbr_sort_tmp / 2;
-		if (!sorting(other, actual, global, 3))
+		if (!send_stack_even(actual, other, global, nbr_sort_tmp))
 			return (0);
 	}
 	else
 	{
-		global->nbr_sort = nbr_sort_tmp / 2 + nbr_sort_tmp % 2;
-		if (!sorting(actual, other, global, 1))
-			return (0);
-		global->nbr_sort = nbr_sort_tmp / 2;
-		if (!sorting(other, actual, global, 2))
+		if (!send_stack_odd(actual, other, global, nbr_sort_tmp))
 			return (0);
 	}
 	global->nbr_sort = nbr_sort_tmp;
-	if (stack == 2)
-		stack_on(actual, other, global, stack);
-	if (stack == 3)
+	if (stack == 2 || stack == 3)
 		stack_on(actual, other, global, stack);
 	return (1);
 }
